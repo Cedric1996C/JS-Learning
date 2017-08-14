5703c97c2b965e278f8cf7e6401bdf8f7dcf19ea
JiaCong <18851280888@163.com>
2017-08-14 09:46:02 +0800

    finish tash8 js

--- javascript/task/javascript/js6.js
+++ javascript/task/javascript/js6.js
@@ -1,43 +1,34 @@
 window.onload=function(){
-	var data = [1,3,4];
+	var data = [];
 
 	var dataBox = document.getElementById("box");
 	var textNum = document.getElementsByTagName('textarea')[0];
 	var buttons = document.getElementsByTagName('input');
-
-	function showData(){
-		while(dataBox.hasChildNodes()){
-			dataBox.removeChild(dataBox.firstChild);
-		}
-		for(var i=0;i<data.length;i++){
-			var newData = document.createElement('div');
-			newData.className = "num";
-			newData.innerHTML = data[i];
-			dataBox.appendChild(newData);
-		}
-	};
 
 	function leftIn(){
 		var words = textNum.value;
 		words = words.replace(/[^0-9a-zA-Z]/g, ' ');
 		var arr = words.split(' ');
 		for(var i=0;i<arr.length;i++){
-			data.unshift(arr[i]);
+			var newData = document.createElement('div');
+			newData.className = "num";
+			data.push(arr[i]);
+			newData.innerHTML = arr[i];
+			dataBox.appendChild(newData);
 		}
-		showData()
 	};
 
 	function check(){
 		var reg = textNum.value;
-		var words = dataBox.childNodes;
-		for(var i=0;i<words.length;i++){
-			
+		if(reg != null && reg.length > 0){
+			dataBox.innerHTML = data.map(function(d){
+				d=d.replace(new RegExp(reg,'g'),'<span class="found">' + reg + '</span>');
+				return '<div class="num">' + d + '</div>'
+			}).join(' ');
 		}
-		showData()
 	};
 
 	buttons[0].onclick = leftIn;
 	buttons[1].onclick = check;
-	showData(); 
 
 }
