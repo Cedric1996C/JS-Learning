f437545ba733cbed15edf31de998627f46f43a7e
JiaCong <18851280888@163.com>
2017-12-16 14:40:56 +0800

    clean workspace

--- webpack-demo/app/main.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import greeter from './greeter';
-import config from './config.json';
-
-render(<greeter />, document.getElementById('root'));
-console.log(config.greetText);
